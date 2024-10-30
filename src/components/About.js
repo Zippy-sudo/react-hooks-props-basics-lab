d30b936ea65af17fea8f5bcf090bf3cdd583d345
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-10-30 14:09:43 +0300

    First Trial

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,26 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function Renderp(props){
+  return(
+    <p>{props}</p>
+  )
+}
+
+function BioInput(props) {
+  if (props) {
+    return Renderp(props)
+  } else return null
+
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {BioInput (props.bio)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
